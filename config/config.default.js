044507c244ccad2414238b2235f5fa35822da624
DBZiou <3224627721@qq.com>
2025-10-12 18:01:21 +0800

    feat: 配置数据库

--- config/config.default.js
+++ config/config.default.js
@@ -1,3 +1,18 @@
 module.exports = {
-  name: 'elpis-demo'
+  name: 'elpis-demo',
+  // 数据库配置
+  db: {
+    client: '',
+    connection: {
+      host: '',
+      port: '',
+      user: '',
+      password: '',
+      database: ''
+    },
+    pool: {
+      min: 2,
+      max: 10
+    }
+  }
 }
